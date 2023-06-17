65346fb109c9b5f69f0c3859f470df7f2ac7f764
McRonaah <ronaldcheruiyot342@gmail.com>
2023-06-18 01:14:37 +0300

    created link file for navbar links

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -1,11 +1,17 @@
 import { useState } from "react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+import Logo from "@/assets/Logo.png";
 
 type Props = {}
 
 const Navbar = (props: Props) => {
+    const flexBetween = "flex items-center justify-between";
   return (
-    <div>Navbar</div>
+    <div>
+        <nav>
+            
+        </nav>
+    </div>
   )
 }
 
