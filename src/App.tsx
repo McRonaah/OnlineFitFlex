5385f7e6106a14ed6fe13887e8de0329a3c67479
McRonaah <ronaldcheruiyot342@gmail.com>
2023-06-18 01:23:46 +0300

    updated navbar menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
 import Navbar from "@/scenes/navbar";
 
+import { useEffect, useState } from "react";
+
 function App() {
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
 
   return (
     <>
       <div className="app bg-gray-20">
       <Navbar
         // isTopOfPage={isTopOfPage}
-        // selectedPage={selectedPage}
-        // setSelectedPage={setSelectedPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
       />
+      <Home setSelectedPage={setSelectedPage} />
+      {/* <Benefits setSelectedPage={setSelectedPage} />
+      <OurClasses setSelectedPage={setSelectedPage} />
+      <ContactUs setSelectedPage={setSelectedPage} /> */}
       </div>
     </>
   )
